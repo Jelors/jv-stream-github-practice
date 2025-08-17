8600d28f1959d07829ac2ac26c98d2fbb88579e9
Andrii Honchar <andriihoncharw@gmail.com>
2025-08-17 11:34:38 +0300

    solution attemp #2

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,13 +4,14 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    static final int MIN_PERIOD_TO_VOTE = 10;
-    static final int MIN_AGE_TO_VOTE = 35;
+    private static final int MIN_PERIOD_TO_VOTE = 10;
+    private static final int MIN_AGE_TO_VOTE = 35;
+    private static final String REQUIRED_NATIONALITY_FOR_VOTE = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         if (candidate.getAge() >= MIN_AGE_TO_VOTE
-                && candidate.getNationality().equals("Ukrainian")
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY_FOR_VOTE)
                 && candidate.calculatePeriodInUkr(candidate.getPeriodsInUkr()) >= MIN_PERIOD_TO_VOTE
                 && candidate.isAllowedToVote()) {
             return true;
